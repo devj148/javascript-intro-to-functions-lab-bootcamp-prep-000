94a983a740539beda947b85cc0f17ee2eb813343
Devin Jones <devinmjones@gmail.com>
2018-04-05 13:46:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 function shout(a){
   a = a.toUpperCase();
-  return a
+  return a;
 }
 
 function whisper(a){
   a = a.toLowerCase();
-  return a
+  return a;
 }
+
+function logShout(a){
+  a = a.toUpperCase();
+  console.log(a);
+}
+
+function logWhisper(a){
+  a = a.toLowerCase();
+  console.log(a);
+}
+
